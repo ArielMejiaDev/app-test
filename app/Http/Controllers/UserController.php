a017813a2446b68f6dddade70e20718e46b4ac1e
arielmejia <arielmejiadev@gmail.com>
2021-08-02 23:26:04 -0600

    hotfix: fix userscontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Charts\MonthlyUsersChart;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function __invoke(MonthlyUsersChart $chart, Request $request)
+    public function __invoke(Request $request)
     {
-        return view('users/index', ['chart' => $chart->build()]);
+        return view('users/index');
     }
 }
